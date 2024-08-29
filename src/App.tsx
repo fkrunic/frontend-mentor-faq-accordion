ed4ee1b0dc1348825cd3a97c27e39edd023a15b9
fkrunic <12419388+fkrunic@users.noreply.github.com>
2024-08-29 15:09:50 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      </div>
+  )
+}
+
+export default App
